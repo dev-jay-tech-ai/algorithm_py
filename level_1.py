402d03a69dadfb6b772b4de05567b5c2e532fcd6
jaewonks <jaewonaaart@gmail.com>
2024-03-11 16:32:58 +0000

    solution2

--- level_1.py
+++ level_1.py
@@ -1,2 +1,7 @@
-def solution(x):
-    return x % sum([int(digit) for digit in str(x)]) == 0
+def solution1(x):
+  return x % sum([int(digit) for digit in str(x)]) == 0
+
+def solution2(a, b): 
+    return sum(range(min(a,b),max(a,b)+1))
+
+print(solution2(3,5))
